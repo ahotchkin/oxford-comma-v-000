a4ab0f7378c38acaaf273d27e15b8ccedda7042e
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-02-17 22:42:22 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
   if array.length == 1
     puts array
-  elsif array.length == 2
-    puts array.join(" and ")
-  elsif array.length >= 3
-    puts array, join(", ")
+  # elsif array.length == 2
+  #   puts array.join(" and ")
+  # elsif array.length >= 3
+  #   puts array, join(", ")
   end
 end
