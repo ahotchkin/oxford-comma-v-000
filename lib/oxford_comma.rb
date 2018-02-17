e9c15067be80a1c16ea1629b611c76cffa7a36f2
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-02-17 22:48:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2
     array.join(" and ")
-  # elsif array.length >= 3
-  #   puts array, join(", ")
+  elsif array.length >= 3
+    puts array, join(", " + "and" array[-1])
   end
 end
